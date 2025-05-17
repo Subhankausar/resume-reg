080a123d9a3c5c2c252417e49972d56df7483e6f
Subhan Kausar <126421273+Subhankausar@users.noreply.github.com>
2025-05-17 23:07:38 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -7,13 +7,7 @@ $pass = getenv("MYSQLPASSWORD");
 
 $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 
-try {
-    $pdo = new PDO($dsn, $user, $pass);
-    echo "✅ Connected to the database successfully!";
-} catch (PDOException $e) {
-    echo "❌ Connection failed: " . $e->getMessage();
-    exit; // Stop execution if connection fails
-}
+
 
 // You can now safely use $pdo here
 ?>
