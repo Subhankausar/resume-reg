37f859b18ca3056a942b9a1ccc2d8be990668e4b
Subhan Kausar <126421273+Subhankausar@users.noreply.github.com>
2025-05-17 20:49:36 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -3,8 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$host = 'containers-us-west-123.railway.app';
-$port = '3306';
+// Railway public MySQL connection details
+$host = 'maglev.proxy.rlwy.net';
+$port = '41197';
 $dbname = 'railway';
 $user = 'root';
 $pass = 'ZcPphICdqmFwpYrASNWpxNZDqxRGiVXZ';
